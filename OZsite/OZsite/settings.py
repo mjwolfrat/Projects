6a1bfa9cea9039958e4681e891b350a2b6558d94
Maarten Wolfrat <mjwolfrat@hotmail.com>
2019-11-19 15:58:33 +0100

    added ledenapp

--- OZsite/OZsite/settings.py
+++ OZsite/OZsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leden',
 ]
 
 # Middleware framework
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Amsterdam'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
