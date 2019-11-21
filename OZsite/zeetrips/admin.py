210b037757cd603b01292d44a3eb00a9dee3c8b5
m.wolfrat <m.wolfrat@WS0108>
2019-11-21 14:53:49 +0100

    Vistrips

--- OZsite/zeetrips/admin.py
+++ OZsite/zeetrips/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Boot, Haven,Geboekt,Vistrip
+
+admin.site.register(Boot)
+admin.site.register(Haven)
+admin.site.register(Vistrip)
+admin.site.register(Geboekt)
--- OZsite/zeetrips/models.py
+++ OZsite/zeetrips/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Haven(models.Model):
+    """Model representing a book genre."""
+    plaatsnaam = models.CharField(max_length=200, help_text='vul plaats van de haven in')
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.plaatsnaam
+
+class Boot(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    naam = models.CharField(max_length=200)
+    haven  = models.ForeignKey('Haven', on_delete=models.SET_NULL, null=True)
+    plaatsen = models.IntegerField(default=0)
+    schipper = models.CharField(max_length=200) 
+
+    class Meta: 
+        verbose_name = "Boot"
+        verbose_name_plural = "Boten"
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.naam
+    
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('boot-details', args=[str(self.id)])
+
+import uuid # Required for unique book instances
+
+
+
+from django.contrib.auth.models import User
+
+class Geboekt(models.Model):
+    naam = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    class Meta: 
+       verbose_name = "Visser"
+       verbose_name_plural = "Vissers"
+
+class Vistrip(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unieke ID voor deze trip')
+    boot = models.ForeignKey('Boot', on_delete=models.SET_NULL, null=True) 
+    datum = models.DateField(null=True, blank=True)
+    geboekt = models.ManyToManyField(Geboekt)
+
+    class Meta:
+        ordering = ['datum']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{boot.naam} ({datum})'
