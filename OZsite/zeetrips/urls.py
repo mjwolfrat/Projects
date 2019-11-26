ca69e7827cd06756c67fea2ba2f0bdc660111c0a
Maarten Wolfrat <mjwolfrat@hotmail.com>
2019-11-26 17:34:58 +0100

    listview

--- OZsite/zeetrips/urls.py
+++ OZsite/zeetrips/urls.py
@@ -4,11 +4,7 @@ from . import views
 from django.urls import include
 
 urlpatterns = [
-     path('', views.index, name='index'),
-]
-#Add Django site authentication urls (for login, logout, password management)
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('vistrips/', views.VistripListView.as_view(), name='vistrips'),
-    path('vistrips/<int:pk>', views.VistripDetailView.as_view(), name='vistrip-detail'),
+    path('', views.index, name='index'),
+    path('vistrips/', views.VistripListView.as_view(), name='vistrip'),
+    path('vistrip/<int:pk>', views.VistripDetailView.as_view(), name='vistrip-detail'),
 ]
