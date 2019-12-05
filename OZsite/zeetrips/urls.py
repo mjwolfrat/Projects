9ac30ff148127a5a570e86671c5f86b42ac1c0f4
m.wolfrat <m.wolfrat@WS0108>
2019-12-05 16:05:23 +0100

    Mijn trips

--- OZsite/zeetrips/urls.py
+++ OZsite/zeetrips/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('vistrips/', views.VistripListView.as_view(), name='vistrip'),
     path('vistrip/<int:pk>', views.VistripDetailView.as_view(), name='vistrip-detail'),
+    path('mijntrips/', views.MijnTripsByUserListView.as_view(), name='mijn-trips'),
 ]
